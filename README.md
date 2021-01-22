8528a88fe095dfdb971156bad65a3e2f9814fe60
Nathan Brown <nathan.a.brown@outlook.com>
2021-01-21 20:16:38 -0500

    created a second scraping script to output all results to a single csv file

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This is a Python-based project which aims to utilize the Python programming language to perform data analysis in an effort to identify what effects, if any, temperature plays in the effectiveness of a knuckleball pitch. This project aims to showcase the use of Python for webscraping as a way to gather data, as well as analyzing the data.
 
-All pitching data for this project was obtained from https://www.baseball-reference.com/ and focuses on the career statistics of former Boston Red Sox pitcher Tim Wakefield, one of Major League Baseball's most well known and successful knuckleball pitchers.
+All pitching data for this project was obtained from <https://www.baseball-reference.com/> and focuses on the career statistics of former Boston Red Sox pitcher Tim Wakefield, one of Major League Baseball's most well known and successful knuckleball pitchers.
--- scrape1.py
+++ scrape1.py
@@ -2,9 +2,6 @@ import csv
 import requests
 from bs4 import BeautifulSoup as bs
 
-
-# Need to write a script to scrape the main page for the player to get table data for years active
-# instead of hard coding it in.
 career_years = ["1992", "1993", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011"]
 
 base_page = "https://www.baseball-reference.com/players/gl.fcgi?id=wakefti01&t=p&year="
@@ -17,17 +14,28 @@ table_id = "#pitching_gamelogs"
 # Any table row within <tbody> that has a class of "thead" can be excluded. The <tfoot> should 
 # also be excluded from the data being sent to the .csv file.
 
-for year in career_years:
-    response = requests.get(base_page + year)
-    soup = bs(response.text, "html.parser")
-    table = soup.select(table_id)
-    rows = table[0].findAll("tr")
-    output = "twake"+year+".csv"
+with open("waket_games.csv", "wt+", newline="") as file:
+    writer = csv.writer(file)
 
-    with open(output, "wt+", newline="") as file:
-        writer = csv.writer(file)
-        for row in rows:
+    for year in career_years:
+        response = requests.get(base_page + year)
+        soup = bs(response.text, "html.parser")
+        table = soup.select(table_id)
+        header = table[0].findAll("thead")
+        header_rows = header[0].findAll("tr")
+        body = table[0].findAll("tbody")
+        body_rows = body[0].findAll("tr")
+
+        for row in header_rows:
             csv_row = []
+            for cell in row.findAll(["td", "th"]):
+                csv_row.append(cell.get_text())
+            writer.writerow(csv_row)
+
+        for row in body_rows:
+            csv_row = []
+            # if row.find(class_="thead"):
+            #     continue           
             for cell in row.findAll(["td", "th"]):
                 csv_row.append(cell.get_text())
             writer.writerow(csv_row)
